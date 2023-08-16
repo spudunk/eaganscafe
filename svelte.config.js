f5ba9a3a04d2da1fbec18fc25016667497debf3d
spudunk <hicksme@gmail.com>
2023-08-16 12:34:17 -0700

    switch to cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,15 +11,26 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		// adapter: adapter()
+
+		// Static adapter
+		// adapter: adapter({
+		// 	 // default options are shown. On some platforms
+    //   // these options are set automatically — see below
+    //   pages: 'build',
+    //   assets: 'build',
+    //   fallback: undefined,
+    //   precompress: false,
+    //   strict: true
+		// })
+
 		adapter: adapter({
-			 // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
-      precompress: false,
-      strict: true
-		})
+      // See below for an explanation of these options
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      }
+    })
+
 	},
 	preprocess: vitePreprocess()
 };
