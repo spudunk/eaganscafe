7bee6f38d0674adfaa1af28cc51dfbca2a7f8623
spudunk <hicksme@gmail.com>
2023-08-17 13:38:30 -0700

    fix footer styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 
 <slot />
 
-<footer class="container w-full mt-4 py-2 text-center print:hidden absolute bottom-0 left-0">
+<footer class="container w-full mt-4 py-2 text-center print:hidden absolute bottom-0 left-1/2 -translate-x-1/2">
   &copy;2023 Eagans Cafe <br />
   <a href="https://icons8.com" class="text-xs opacity-50">credit icons8.com</a>
 </footer>
