8f6dd77954456a9b7ba816eb433bb4a1adcfbf04
spudunk <hicksme@gmail.com>
2023-08-20 09:01:38 -0700

    add tabs

---
+++ src/lib/components/TabButton.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { tab } from "$lib/stores";
+
+  export let id: string;
+
+  let selectedTab: string;
+
+  tab.subscribe((value) => {
+    selectedTab = value;
+  });
+</script>
+
+<button
+  on:click={() => {
+    tab.set(id);
+  }}
+  class={`py-1 px-2 rounded-t font-bold ${
+    selectedTab == id ? "border-t border-r border-l" : "text-neutral-500"
+  }`}
+>
+  <slot />
+</button>
--- src/routes/ethel/+page.svelte
+++ src/routes/ethel/+page.svelte
@@ -2,13 +2,22 @@
   // @ts-nocheck
   import { ethelLunchMenu, ethelBreakfastMenu } from "$lib/menu";
   import { ethelInfo } from "$lib/info";
-  import MenuSection from "../../components/MenuSection.svelte";
-  import Header from "../../components/Header.svelte";
-  import SizeButton from "../../components/SizeButton.svelte";
-  import SEO from "../../components/SEO.svelte";
-  import Info from "../../components/Info.svelte";
+  import { tab } from "$lib/stores";
+  import MenuSection from "$lib/components/MenuSection.svelte";
+  import Header from "$lib/components/Header.svelte";
+  import SizeButton from "$lib/components/SizeButton.svelte";
+  import SEO from "$lib/components/SEO.svelte";
+  import Info from "$lib/components/Info.svelte";
+  import TabButton from "$lib/components/TabButton.svelte";
+
   const lunchSections = Object.keys(ethelLunchMenu);
   const breakfastSections = Object.keys(ethelBreakfastMenu);
+
+  let selectedTab;
+  tab.subscribe((value) => {
+    selectedTab = value;
+  })
+
 </script>
 
 <SEO url="https://eaganscafe.com/ethel" title="Ethel Eagans Cafe" />
@@ -19,16 +28,28 @@
   <Info info={ethelInfo} />
   <SizeButton />
 
-  <section id="lunch_menu" class="pt-8 grid gap-8 justify-center">
-    {#each lunchSections as section}
-      <MenuSection menu={ethelLunchMenu[section]} />
-    {/each}
-  </section>
+  <!-- Tabs -->
+  <div class="flex gap-2 text-xl mt-8 px-2 border-b">
+    <TabButton id="lunch">Lunch</TabButton>
+    <TabButton id="breakfast">Breakfast</TabButton>
+  </div>
 
-  <section id="breakfast_menu" class="pt-8 grid gap-8 justify-center break-inside-avoid">
-    {#each breakfastSections as section}
-      <MenuSection menu={ethelBreakfastMenu[section]} />
-    {/each}
-  </section>
-
+  {#if selectedTab == "lunch"}
+    <!-- Lunch -->
+    <section id="lunch" class="pt-8 grid gap-8 justify-center">
+      {#each lunchSections as section}
+        <MenuSection menu={ethelLunchMenu[section]} />
+      {/each}
+    </section>
+  {:else if selectedTab == "breakfast"}
+    <!-- Breakfast -->
+    <section
+      id="breakfast"
+      class="pt-8 grid gap-8 justify-center break-inside-avoid"
+    >
+      {#each breakfastSections as section}
+        <MenuSection menu={ethelBreakfastMenu[section]} />
+      {/each}
+    </section>
+  {/if}
 </main>
