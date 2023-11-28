90108b97bbb2c8f811b997ebb9d62a61224e4918
Christopher Hicks <hickscme@gmail.com>
2023-11-28 14:05:47 -0800

    get type text

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -2,7 +2,7 @@ import { json } from "@sveltejs/kit";
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ platform }) {
-  const info = await platform?.env?.KV.get("info", { type: "string" });
+  const info = await platform?.env?.KV.get("info", { type: "text" });
   console.log("+server.js info: ", info);
   return json(info);
 }
