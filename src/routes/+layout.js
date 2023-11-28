e3696f7ca93c99497c3824963ccb09586fe7f8f7
Christopher Hicks <hickscme@gmail.com>
2023-11-28 14:34:37 -0800

    disable logging

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,7 +1,6 @@
 export async function load({ fetch }) {
 	const res = await fetch(`/api`);
 	const info = await res.json();
-	console.log("+layout.js info: ", info)
-
+	// console.log("+layout.js info: ", info)
 	return info ;
 }
