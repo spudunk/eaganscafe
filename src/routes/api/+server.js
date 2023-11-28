bf0780bb2213f83aff61b2993ca5fea4126df08e
Christopher Hicks <hickscme@gmail.com>
2023-11-28 14:33:21 -0800

    remove KV put

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,13 +1,10 @@
 import { json } from "@sveltejs/kit";
 
-import { info as libInfo } from "$lib/info";
+// import { info as libInfo } from "$lib/info";
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ platform }) {
-  const k = await platform?.env?.KV.put("info", JSON.stringify(libInfo))
+  // const k = await platform?.env?.KV.put("info", JSON.stringify(libInfo))
   const info = await platform?.env?.KV.get("info", { type: "json" });
-  // const info = await platform?.env?.KV.get("info");
-  
-  console.log("+server.js info: ", info);
   return json(info);
 }
