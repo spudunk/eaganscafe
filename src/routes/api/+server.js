cede30040bb6f43fa57925d035675002f60dcfbd
Christopher Hicks <hickscme@gmail.com>
2023-11-28 14:03:13 -0800

    get kv as string

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -2,7 +2,7 @@ import { json } from "@sveltejs/kit";
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ platform }) {
-  const info = await platform?.env?.KV.get("info", { type: "json" });
+  const info = await platform?.env?.KV.get("info", { type: "string" });
   console.log("+server.js info: ", info);
   return json(info);
 }
