1f4d7bd798668e28c3ab9375768ec90c14e8ccae
Christopher Hicks <hickscme@gmail.com>
2023-11-28 13:14:42 -0800

    remove error handling

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,14 +1,8 @@
 // import { info } from "$lib/info";
 
 /** @type {import('./$types').LayoutServerLoad} */
-export async function load(context) {
-  try {
-    return {
-      info: await context.platform?.env?.KV.get("info", { type: "json" }),
-    };
-  } catch (error) {
-    console.error(error);
-    // return { info: info };
-    return {info: {}};
-  }
+export async function load({platform}) {
+  return {
+    info: await platform?.env?.KV.get("info", { type: "json" }),
+  };
 }
