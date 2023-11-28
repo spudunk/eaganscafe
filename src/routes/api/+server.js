bcece3c10325a4817f2de9fd37ed838404dde6ab
Christopher Hicks <hickscme@gmail.com>
2023-11-28 14:19:29 -0800

    test

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -2,7 +2,9 @@ import { json } from "@sveltejs/kit";
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ platform }) {
-  const info = await platform?.env?.KV.get("info", { type: "json" });
+  // const info = await platform?.env?.KV.get("info", { type: "json" });
+  const info = await platform?.env?.KV.get("info");
+  
   console.log("+server.js info: ", info);
-  return json(String(info));
+  return new Response(info);
 }
