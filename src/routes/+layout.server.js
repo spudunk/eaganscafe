8740e3c63b38f1600a57511ba7ffab535990ebde
Christopher Hicks <hickscme@gmail.com>
2023-11-28 13:02:38 -0800

    return info object from import on dev

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,11 +1,13 @@
+import { info } from "$lib/info";
+
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load(context) {
   try {
     return {
-      info: await context.platform?.env?.KV.get("info", {type: "json"}),
+      info: await context.platform?.env?.KV.get("info", { type: "json" }),
     };
   } catch (error) {
     console.error(error);
-    return {};
+    return { info: info };
   }
 }
