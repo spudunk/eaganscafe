4616f1908dca8a69d880c8eb614d6711250ed2b6
Christopher Hicks <hickscme@gmail.com>
2023-11-28 15:36:13 -0800

    bug fix

--- src/lib/components/SEO.svelte
+++ src/lib/components/SEO.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import { info } from "$lib/info";
-  export let description = info.description; // prop but use lib/info as default
-  export let title = info.title;
-  export let url = info.url;
+  import data from "$lib/data";
+  export let description = data.description; // prop but use lib/info as default
+  export let title = data.title;
+  export let url = data.url;
   export let img = "";
 </script>
 
