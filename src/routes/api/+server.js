ebc7558b6615af37ebd5426623ac0361c529e4e4
Christopher Hicks <hickscme@gmail.com>
2023-11-28 14:25:54 -0800

    write json first

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,9 +1,12 @@
 import { json } from "@sveltejs/kit";
 
+import { info as libInfo } from "$lib/info";
+
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ platform }) {
-  // const info = await platform?.env?.KV.get("info", { type: "json" });
-  const info = await platform?.env?.KV.get("info");
+  const k = await platform?.env?.KV.put("info", libInfo)
+  const info = await platform?.env?.KV.get("info", { type: "json" });
+  // const info = await platform?.env?.KV.get("info");
   
   console.log("+server.js info: ", info);
   return new Response(info);
