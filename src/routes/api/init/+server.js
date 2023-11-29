998984c74aa7a32c2732132cc758efd9ea100b12
Christopher Hicks <hickscme@gmail.com>
2023-11-28 18:17:05 -0800

    remove init route

--- src/routes/api/init/+server.js
+++
@@ -1,7 +0,0 @@
-import { json } from "@sveltejs/kit";
-import data from "$lib/data";
-
-export async function GET({ platform }) {
-  const res = await platform?.env?.KV.put("data", JSON.stringify(data));
-  return json({ "KV Response": res });
-}
