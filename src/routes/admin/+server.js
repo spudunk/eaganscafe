71e99bc218fb3e1ac435c407e17179205a6508e7
Christopher Hicks <hickscme@gmail.com>
2023-12-01 16:33:37 -0800

    refactor

--- src/routes/admin/+server.js
+++ src/routes/admin/+server.js
@@ -3,12 +3,9 @@ import { json } from "@sveltejs/kit";
 export async function POST({ platform, request }) {
   const body = await request.json();
   // console.log('request json: ', body)  // okay here
-  if (platform?.env && body) {
+  if (platform?.env) {
     const res = await platform?.env?.KV.put("data", JSON.stringify(body));
     return json({ message: `saved` });
   }
-  if (body) {
-    return json({ message: "no platform" });
-  }
-  return json({ message: "no request.body" });
+  return json({ message: `fail, no platform.env` });
 }
