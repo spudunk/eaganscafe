95acf114bcea643cb344e708b5d0238fa690a16e
Christopher Hicks <hickscme@gmail.com>
2023-12-01 17:06:53 -0800

    update message

--- src/routes/admin/+server.js
+++ src/routes/admin/+server.js
@@ -5,7 +5,7 @@ export async function POST({ platform, request }) {
   // console.log('request json: ', body)  // okay here
   if (platform?.env) {
     const res = await platform?.env?.KV.put("data", JSON.stringify(body));
-    return json({ message: `saved` });
+    return json({ message: `saved!` });
   }
   return json({ message: `save fail: platform.env not available` });
 }
