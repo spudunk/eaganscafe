0c8b0ae89b38029511392a18d5a024615285b004
Christopher Hicks <hickscme@gmail.com>
2023-12-03 13:08:36 -0800

    fix wrangler config

--- svelte.config.js
+++ svelte.config.js
@@ -23,12 +23,14 @@ const config = {
     //   strict: true
 		// })
 
+    // Cloudflare adapter
 		adapter: adapter({
       // See below for an explanation of these options
       routes: {
         include: ['/*'],
         exclude: ['<all>']
-      }
+      },
+      config: 'wrangler.toml'
     })
 
 	},
