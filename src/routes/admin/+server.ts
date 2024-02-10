2fa1bb5d846f0a07af8cb6b859a0cbf145bd282f
Christopher Hicks <hickscme@gmail.com>
2024-02-10 11:21:54 -0800

    add update time

--- src/routes/admin/+server.ts
+++ src/routes/admin/+server.ts
@@ -1,10 +1,12 @@
 import { json } from "@sveltejs/kit";
+import type { Data } from "$lib/types.js";
 
 export async function POST({ platform, request }) {
-  const body = await request.json();
+  const data = await request.json() as Data;
   // console.log('request json: ', body)  // okay here
   if (platform?.env) {
-    const res = await platform?.env?.KV.put("data", JSON.stringify(body));
+    data.updated = new Date().getTime();
+    await platform?.env?.KV.put("data", JSON.stringify(data));
     return json({ message: `saved!` });
   }
   return json({ message: `save fail: platform.env not available` });
